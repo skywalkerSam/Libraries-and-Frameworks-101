12b215930bf34ee001387dafb6224bec18166537
Starboy <samskywalker001@gmail.com>
2026-08-07 18:37:02 +0530

    footer

--- EXERCISES/01-reusable-mega-navbar/src/App.jsx
+++ EXERCISES/01-reusable-mega-navbar/src/App.jsx
@@ -1,10 +1,13 @@
 import { Navbar } from "./Navbar";
 import "./App.css"
+import { Footer } from "./Footer";
 
 export default function App() {
   return (
     <>
       <Navbar></Navbar>
+      <main></main>
+      <Footer></Footer>
     </>
   );
 }
---
+++ EXERCISES/01-reusable-mega-navbar/src/Footer.jsx
@@ -0,0 +1,31 @@
+export function Footer() {
+  return (
+    <footer>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>{" "}
+        <li>
+          <a href="#">Services</a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a href="#">Sales</a>
+        </li>{" "}
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a href="#">Privacy Policy</a>
+        </li>{" "}
+        <li>
+          <a href="#">Terms & Conditions</a>
+        </li>
+      </ul>
+      <p>&copy; 12026 Starboy inc. All Rights Dissolved.</p>
+    </footer>
+  );
+}
